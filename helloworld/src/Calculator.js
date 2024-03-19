59a2520655ca1990b7ad82a1b37fc7d98e0739d1
sushant manjare <sushantmanjare17269@gmail.com>
2024-03-19 23:02:24 +0530

    sample react

---
+++ helloworld/src/Calculator.js
@@ -0,0 +1,10 @@
+
+export function add(num1 , num2) {
+    return num1+num2;
+}
+export function Sub(num1 , num2) {
+    return num1-num2;
+}
+export default function abc(){
+    return 100;
+}
---
+++ helloworld/src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+    <div>This is Header</div>
+    </header>
+  )
+}
